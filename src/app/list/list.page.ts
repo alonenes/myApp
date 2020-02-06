f0e93f575320a5f008fb97c1c61cb89a7b56ce1d
alonenes <alonenes.00@hotmail.com>
2020-02-05 22:27:22 -0800

    Test push

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { DocumentViewer, DocumentViewerOptions } from '@ionic-native/document-viewer/ngx';
+import { File } from '@ionic-native/file/ngx';
+import { FileTransfer } from '@ionic-native/file-transfer/ngx';
+import { Platform } from '@ionic/angular';
+
+
 
 @Component({
   selector: 'app-list',
@@ -20,7 +26,7 @@ export class ListPage implements OnInit {
     'build'
   ];
   public items: Array<{ title: string; note: string; icon: string }> = [];
-  constructor() {
+  constructor(private platform: Platform,private document: DocumentViewer,private file: File,private transfer: FileTransfer) {
     for (let i = 1; i < 11; i++) {
       this.items.push({
         title: 'Item ' + i,
@@ -29,6 +35,33 @@ export class ListPage implements OnInit {
       });
     }
   }
+
+  openLocalPdf() {
+    const option: DocumentViewerOptions = {
+        title: 'My PDF'
+    };
+    this.document.viewDocument('assets/D5100_EN.pdf', 'application/pdf', option);
+  }
+
+  downloadAndOpenPdf() {
+    let path = null;
+    
+    if (this.platform.is('ios')) {
+       path = this.file.documentsDirectory;
+    } else {
+       path = this.file.dataDirectory;
+    }
+    
+    const transfer = this.transfer.create();
+    transfer.download('https://cdn-10.nikon-cdn.com/pdf/manuals/dslr/D5100_EN.pdf', path + 'myfile.pdf').then(entry => {
+      let url = entry.toURL();
+      this.document.viewDocument(url, 'application/pdf',{});
+    })
+    
+      
+  }
+
+  
 
   ngOnInit() {
   }
